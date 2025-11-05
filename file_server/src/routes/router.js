6bbab8d5e89567d4d75f8398a5f8ffa962c8b31f
Ahamadi arshiya <msarshiya04@gmail.com>
2025-11-04 23:28:36 -0800

    working on shipping api

--- file_server/src/routes/router.js
+++ file_server/src/routes/router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { register, login } = require('../controllers/users/userController');
 const { products, singleProduct }  = require("../controllers/products/products")
-
+const { createShipping } = require("../controllers/shipping/shippingController")
 
 
 
@@ -11,7 +11,8 @@ const { products, singleProduct }  = require("../controllers/products/products")
 router.post('/user/register', register);
 router.post('/user/login', login);
 router.get('/products/all-products', products);
-router.get('/products/singleProduct/:id', singleProduct)
+router.get('/products/singleProduct/:id', singleProduct);
+router.post('/shipping/add-shippingInfo', createShipping )
 
 
 module.exports = router;
