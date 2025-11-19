d4c4f714d6f946ebdb67f980b58a92d9c3fc5f56
Amujuri Hema Sai <hemasaiamujuri3@gmail.com>
2025-11-18 23:29:19 -0800

    install cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const app = express()
 const db = require('./src/config/db')
+const cors = require("cors");
 const routers = require("./src/routes/router")
 
 
 const PORT = 5000;
 
+app.use(cors())
 app.use(express.json())
 
 
